e30b7c16bc8c5d03d460cdb8a82df423032cb3ae
anzohra@outlook.com <anzohra@outlook.com>
2018-04-04 18:14:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array.pop()
 }
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length-1)
+  array.slice(0, array.length- 1)
 }
 
 
